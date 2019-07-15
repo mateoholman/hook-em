3b8539d67348181073c35b3437d69709f3be2ce4
Matthew Holman <mateoholman@gmail.com>
2019-07-15 10:22:33 -0500

    Create form to demo useState hook

--- src/views/UseState.jsx
+++ src/views/UseState.jsx
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField'
 
 const UseState = () => {
-  const [coolKids, setCoolKids] = useState();
-  //TODO: Add Material UI so you can use those components.
-  //TODO: Add input/button for adding cool kids.
+  const [newCoolKid, setNewCoolKid] = useState('');
+  const [coolKids, setCoolKids] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setCoolKids([...coolKids, newCoolKid]);
+    setNewCoolKid('');
+  };
+
   return (
-    <>
+    <section>
       <p>useState is a hook!</p>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-name"
+          label="Name"
+          value={newCoolKid}
+          onChange={e => setNewCoolKid(e.target.value)}
+          margin="normal"
+          variant="outlined"
+        />
+      </form>
       <p>Cool Kids Club</p>
-    </>
+      <ul>
+        {coolKids.map((coolKid, index) => (
+          <li key={index}>{coolKid}</li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
