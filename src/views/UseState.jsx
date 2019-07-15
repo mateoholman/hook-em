56e3e66ed151e94775ea673ea6b4b1fb7c6638ca
Matthew Holman <mateoholman@gmail.com>
2019-07-15 09:35:04 -0500

    Setup material UI

--- src/views/UseState.jsx
+++ src/views/UseState.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const UseState = () => (
-  <p>useState is a hook!</p>
-);
+const UseState = () => {
+  const [coolKids, setCoolKids] = useState();
+  //TODO: Add Material UI so you can use those components.
+  //TODO: Add input/button for adding cool kids.
+  return (
+    <>
+      <p>useState is a hook!</p>
+      <p>Cool Kids Club</p>
+    </>
+  );
+};
 
 export default UseState;
